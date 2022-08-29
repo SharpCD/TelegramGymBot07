06316c1dbcb9e13a327e65494984dfb9cc7ae609
Nikita.L <nikita.khala09052007@gmail.com>
2022-08-29 23:23:58 +0300

    mda

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def start_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Начать")
     markup.add(item1)
-    bot.send_message(message.chat.id,'Пока, с помощью этого бота ты сможешь ставить напоминалки, давай начнём уже!',reply_markup=markup)
+    bot.send_message(message.chat.id,', с помощью этого бота ты сможешь ставить напоминалки, давай начнём уже!',reply_markup=markup)
 
 
 
